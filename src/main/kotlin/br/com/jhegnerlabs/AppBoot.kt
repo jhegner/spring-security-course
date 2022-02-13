dcd5bb866c33ec086fb29dab5e85303b28db88da
jhegner <jidlafehegner@gmail.com>
2022-02-12 22:43:03 -0300

    renomeado classe boot da app

--- src/main/kotlin/br/com/jhegnerlabs/AppBoot.kt
+++ src/main/kotlin/br/com/jhegnerlabs/AppBoot.kt
@@ -4,8 +4,8 @@ import org.springframework.boot.autoconfigure.SpringBootApplication
 import org.springframework.boot.runApplication
 
 @SpringBootApplication
-class SpringSecurityCourseApplication
+class AppBoot
 
 fun main(args: Array<String>) {
-	runApplication<SpringSecurityCourseApplication>(*args)
+	runApplication<AppBoot>(*args)
 }
