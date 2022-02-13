b8020f2fbdc55aa7755d3ce022560d9decb2ce4d
jhegner <jidlafehegner@gmail.com>
2022-02-12 22:35:01 -0300

    inicio do repo

---
+++ src/main/kotlin/br/com/jhegnerlabs/SpringSecurityCourseApplication.kt
@@ -0,0 +1,11 @@
+package br.com.jhegnerlabs
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class SpringSecurityCourseApplication
+
+fun main(args: Array<String>) {
+	runApplication<SpringSecurityCourseApplication>(*args)
+}
